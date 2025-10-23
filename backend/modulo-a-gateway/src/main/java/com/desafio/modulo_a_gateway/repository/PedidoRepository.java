05e37f9d80d04face4fcedddd5fa616ba718c2c8
Weslley Ribeiro <weslleysilvaribeiro35@gmail.com>
2025-10-22 23:04:46 -0300

    feat: Cria entidade Pedido com mapeamento JPA e Lombok

---
+++ backend/modulo-a-gateway/src/main/java/com/desafio/modulo_a_gateway/repository/PedidoRepository.java
@@ -0,0 +1,10 @@
+package com.desafio.modulo_a_gateway.repository;
+
+import com.desafio.modulo_a_gateway.model.Pedido;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PedidoRepository extends JpaRepository<Pedido, Long> {
+
+}
---
+++ backend/modulo-b-processor/src/main/java/com/desafio/modulo_b_processor/model/Pedido.java
@@ -0,0 +1,54 @@
+package com.desafio.modulo_b_processor.model;
+
+import org.hibernate.annotations.CreationTimestamp;
+import jakarta.persistence.*;
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+enum Status {
+    RECEBIDO,
+    ENTREGUE
+}
+
+@Entity
+@Table(name = "pedidos")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Pedido {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "tipo_lanche", length = 20)
+    private String tipoLanche;
+
+    @Column(length = 20)
+    private String proteina;
+
+    @Column(length = 20)
+    private String acompanhamento;
+
+    private Integer quantidade;
+
+    @Column(length = 20)
+    private String bebida;
+
+    @Column(name = "valor", precision = 10, scale = 2)
+    private BigDecimal valor;
+
+    @Enumerated(EnumType.STRING)
+    @Column(length = 20)
+    private Status status;
+
+    @Column(name = "criado_em")
+    @CreationTimestamp
+    private Timestamp criadoEm;
+
+}
